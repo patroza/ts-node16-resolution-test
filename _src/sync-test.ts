7765350b608de1725cd30eeaa097d34a080ca437
Patrick Roza <contact@patrickroza.com>
2022-06-22 12:19:04 +0200

    Enhance case.

--- _src/sync-test.ts
+++ _src/sync-test.ts
@@ -1,3 +1,7 @@
 import * as Sync from "@effect-ts/core/Sync"
+import * as Effect from "@effect-ts/core/Effect"
 
-export const abc = Sync.struct({ a: Sync.succeed(1), b: Sync.succeed(2) })
+// Resolves to `any`
+export const fails = Sync.struct({ a: Sync.succeed(1), b: Sync.succeed(2) })
+// has correct type
+export const works = Effect.struct({ a: Sync.succeed(1), b: Sync.succeed(2) })
