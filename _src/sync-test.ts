862c53b2fa49f709658c0aad440eb09c613d8b36
Patrick Roza <contact@patrickroza.com>
2022-06-22 12:21:50 +0200

    Improve issue doc.

--- _src/sync-test.ts
+++ _src/sync-test.ts
@@ -1,7 +1,7 @@
 import * as Sync from "@effect-ts/core/Sync"
 import * as Effect from "@effect-ts/core/Effect"
 
-// Resolves to `any`
+// Resolves to `any` when "Node16" resolution is enabled in `tsconfig.json`. but resolves correctly with "node"
 export const fails = Sync.struct({ a: Sync.succeed(1), b: Sync.succeed(2) })
 // has correct type
 export const works = Effect.struct({ a: Sync.succeed(1), b: Sync.succeed(2) })
